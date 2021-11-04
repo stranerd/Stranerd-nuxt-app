6c302c39318de6dbbea4f6f89adc7a3fe6393c36
kevinand11 <kevinfizu@gmail.com>
2021-11-04 20:01:30 +0100

    fix: cleanup

--- src/application/plugins/capacitor.ts
+++ src/application/plugins/capacitor.ts
@@ -1,8 +1,8 @@
-import { Toast } from "@capacitor/toast";
-import { Network } from "@capacitor/network";
-import { defineNuxtPlugin } from "@nuxtjs/composition-api";
+import { Toast } from '@capacitor/toast'
+import { Network } from '@capacitor/network'
+import { defineNuxtPlugin } from '@nuxtjs/composition-api'
 
-export default defineNuxtPlugin(async ({ app }, inject) => {
-	inject("toast", Toast);
-	inject("network", Network);
-});
+export default defineNuxtPlugin(async (_, inject) => {
+	inject('toast', Toast)
+	inject('network', Network)
+})
--- src/application/plugins/parseLoggedInUser.ts
+++ src/application/plugins/parseLoggedInUser.ts
@@ -1,24 +1,24 @@
-import { defineNuxtPlugin } from "@nuxtjs/composition-api";
-import { useAuth } from "@app/hooks/auth/auth";
-import { ACCESS_TOKEN_NAME, REFRESH_TOKEN_NAME } from "@utils/constants";
-import { parseCookie } from "@utils/cookie";
-import { GetAuthUser } from "@modules/auth";
-import { saveTokens } from "@utils/tokens";
+import { defineNuxtPlugin } from '@nuxtjs/composition-api'
+import { useAuth } from '@app/hooks/auth/auth'
+import { ACCESS_TOKEN_NAME, REFRESH_TOKEN_NAME } from '@utils/constants'
+import { parseCookie } from '@utils/cookie'
+import { GetAuthUser } from '@modules/auth'
+import { saveTokens } from '@utils/tokens'
 
 export default defineNuxtPlugin(async ({ redirect }) => {
-	const cookies = parseCookie(document.cookie ?? "");
+	const cookies = parseCookie(document.cookie ?? '')
 	const {
 		[ACCESS_TOKEN_NAME]: accessToken = null,
 		[REFRESH_TOKEN_NAME]: refreshToken = null
-	} = cookies;
+	} = cookies
 	if (accessToken && refreshToken) {
-		await saveTokens({ accessToken, refreshToken });
-		const user = await GetAuthUser.call().catch(() => null);
+		await saveTokens({ accessToken, refreshToken })
+		const user = await GetAuthUser.call().catch(() => null)
 		if (user) {
-			await useAuth().setAuthUser(user);
-			if (user && !user.isVerified) await redirect("/auth/verify");
+			await useAuth().setAuthUser(user)
+			if (user && !user.isVerified) await redirect('/auth/verify')
 		} else {
-			await saveTokens({ accessToken: null, refreshToken: null });
+			await saveTokens({ accessToken: null, refreshToken: null })
 		}
 	}
-});
+})
